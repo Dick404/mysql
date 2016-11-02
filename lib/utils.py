2dc5b5de246155a5850351eada3e7d91ae3babc2
gophic <gophic@icloud.com>
2016-11-02 17:39:28 +0800

    make the Executor and update the Common;

--- lib/utils.py
+++ lib/utils.py
@@ -4,25 +4,31 @@ import shell
 
 class Common(object):
 
+    parser = None
+    options = None
+    args = None
+
     @classmethod
     def create_parser(cls):
-        parser = OptionParser()
-        parser.add_option("-p", "--path",
-                          action="store_true",
-                          dest="address",
-                          default="/opt/mysql/",
-                          help="indicate the location of mysql source code")
-        (options, args) = parser.parse_args()
-        if options.address == "/opt/mysql/":
-            print "yes"
-        print args
+        cls.parser = OptionParser()
+        cls.parser.add_option("-p", "--path",
+                              action="store",
+                              dest="address",
+                              default="/opt/mysql/",
+                              help="indicate the location of mysql source code")
+        (cls.options, cls.args) = cls.parser.parse_args()
+#      if options.address == "/opt/mysql/":
+#            print "yes"
 
     @classmethod
     def config(cls, path):
         if type(path) != 'str':
             raise BaseException("wrong path for mysql source code.")
 
-
+    @classmethod
+    def check(cls):
+        if cls.args == "" or cls.options == "":
+            cls.parser.help
 
 if __name__ == '__main__':
     c = Common()
